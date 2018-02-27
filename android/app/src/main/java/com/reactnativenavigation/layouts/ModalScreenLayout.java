chri55teffey/react-native-navigation-update

commit b1b5c9291548e762727a2a37b9838b65ce01ec29

[Android] Add ability to start app with deep stack (#2761)
For example, the following code will start a single screen app with two
screens pushed into the stack.

```js
Navigation.startSingleScreenApp({
    components: [
    {
      screen: 'example.Types',
      title: 'Navigation Types',
      overrideBackPress: true
    },
    {
      screen: 'example.Types.CustomButtonScreen',
      title: 'Custom Buttons'
    }
  ]
});
```

android/app/src/main/java/com/reactnativenavigation/layouts/SingleScreenLayout.java
@@ -85,11 +85,18 @@ public class SingleScreenLayout extends BaseLayout {
         stack = new ScreenStack(getActivity(), parent, screenParams.getNavigatorId(), this);
         LayoutParams lp = new LayoutParams(MATCH_PARENT, MATCH_PARENT);
         pushInitialScreen(lp);
+        pushAdditionalScreens(lp);
+        stack.show(NavigationType.Push);
     }
 
     protected void pushInitialScreen(LayoutParams lp) {
         stack.pushInitialScreen(screenParams, lp);
-        stack.show(NavigationType.Push);
+    }
+
+    private void pushAdditionalScreens(LayoutParams lp) {
+        for (ScreenParams screen : screenParams.screens) {
+            stack.pushInitialScreen(screen, lp);
+        }
     }
 
     private void sendScreenChangedEventAfterInitialPush() {

android/app/src/main/java/com/reactnativenavigation/layouts/BottomTabsLayout.java
@@ -106,6 +106,9 @@ public class BottomTabsLayout extends BaseLayout implements AHBottomNavigation.O
         ScreenParams screenParams = params.tabParams.get(position);
         ScreenStack newStack = new ScreenStack(getActivity(), getScreenStackParent(), screenParams.getNavigatorId(), this);
         newStack.pushInitialScreen(screenParams, createScreenLayoutParams(screenParams));
+        for (ScreenParams screen : screenParams.screens) {
+            newStack.pushInitialScreen(screen, createScreenLayoutParams(screen));
+        }
         screenStacks[position] = newStack;
     }
 

android/app/src/main/java/com/reactnativenavigation/params/ScreenParams.java
@@ -1,11 +1,13 @@
 package com.reactnativenavigation.params;
 
+import java.util.Collections;
 import java.util.List;
 
 public class ScreenParams extends BaseScreenParams {
     public String tabLabel;
     public List<PageParams> topTabParams;
     public List<String> sharedElementsTransitions;
+    public List<ScreenParams> screens = Collections.EMPTY_LIST; // used to init a stack with multiple screens
 
     public boolean hasTopTabs() {
         return topTabParams != null && !topTabParams.isEmpty();

android/app/src/main/java/com/reactnativenavigation/params/parsers/ScreenParamsParser.java
@@ -42,6 +42,9 @@ public class ScreenParamsParser extends Parser {
         result.leftButton = ButtonParser.parseLeftButton(params);
 
         result.topTabParams = parseTopTabs(params);
+        if (hasKey(params, "screens")) {
+            result.screens = parseScreens(params.getBundle("screens"));
+        }
 
         if (hasKey(params, FRAGMENT_CREATOR_CLASS_NAME)) {
             result.fragmentCreatorClassName = params.getString(FRAGMENT_CREATOR_CLASS_NAME);
@@ -99,6 +102,15 @@ public class ScreenParamsParser extends Parser {
 
     List<ScreenParams> parseTabs(Bundle params) {
         return parseBundle(params, new ParseStrategy<ScreenParams>() {
+            @Override
+            public ScreenParams parse(Bundle screen) {
+                return ScreenParamsParser.parse(screen);
+            }
+        });
+    }
+
+    private static List<ScreenParams> parseScreens(Bundle screens) {
+        return new Parser().parseBundle(screens, new ParseStrategy<ScreenParams>() {
             @Override
             public ScreenParams parse(Bundle screen) {
                 return ScreenParamsParser.parse(screen);

android/app/src/main/java/com/reactnativenavigation/screens/ScreenStack.java
@@ -95,7 +95,12 @@ public class ScreenStack {
     public void pushInitialScreen(ScreenParams initialScreenParams, LayoutParams params) {
         Screen initialScreen = ScreenFactory.create(activity, initialScreenParams, leftButtonOnClickListener);
         initialScreen.setVisibility(View.INVISIBLE);
+        removeCurrentScreen();
         addScreen(initialScreen, params);
+    }
+
+    private void removeCurrentScreen() {
+        if (!stack.empty()) parent.removeView(peek());
     }
 
     public void push(final ScreenParams params, LayoutParams layoutParams, Promise onPushComplete) {

src/deprecated/platformSpecificDeprecated.android.js
@@ -11,32 +11,45 @@ const resolveAssetSource = require('react-native/Libraries/Image/resolveAssetSou
 import * as newPlatformSpecific from './../platformSpecific';
 
 async function startSingleScreenApp(params) {
-  const screen = params.screen;
-  if (!screen.screen) {
+  const components = params.components;
+  if (!params.screen && !components) {
     console.error('startSingleScreenApp(params): screen must include a screen property');
     return;
   }
+
+  if (components) {
+    params.screen = createSingleScreen(components[0]);
+    components.shift();
+    params.screen.screens = components.map(createSingleScreen);
+  } else {
+    params.screen = createSingleScreen(params.screen);
+  }
+
+  params.sideMenu = convertDrawerParamsToSideMenuParams(params.drawer);
+  params.overrideBackPress = params.screen.overrideBackPress;
+  params.animateShow = convertAnimationType(params.animationType);
+  params.appStyle = convertStyleParams(params.appStyle);
+    if (params.appStyle) {
+      params.appStyle.orientation = getOrientation(params);
+    }
+
+  return await newPlatformSpecific.startApp(params);
+}
+
+function createSingleScreen(params) {
+  let screen = params;
   addNavigatorParams(screen);
   addNavigatorButtons(screen, params.drawer);
   addNavigationStyleParams(screen);
   screen.passProps = params.passProps;
-
   /*
    * adapt to new API
    */
   adaptTopTabs(screen, screen.navigatorID);
   screen.screenId = screen.screen;
-  params.screen = adaptNavigationStyleToScreenStyle(screen);
-  params.screen = adaptNavigationParams(screen);
-  params.appStyle = convertStyleParams(params.appStyle);
-  if (params.appStyle) {
-    params.appStyle.orientation = getOrientation(params);
-  }
-  params.sideMenu = convertDrawerParamsToSideMenuParams(params.drawer);
-  params.overrideBackPress = screen.overrideBackPress;
-  params.animateShow = convertAnimationType(params.animationType);
-
-  return await newPlatformSpecific.startApp(params);
+  screen = adaptNavigationStyleToScreenStyle(screen);
+  screen = adaptNavigationParams(screen);
+  return screen;
 }
 
 function getOrientation(params) {
@@ -297,23 +310,18 @@ async function startTabBasedApp(params) {
   params.tabs = _.cloneDeep(params.tabs);
 
   params.tabs.forEach(function(tab, idx) {
-    addNavigatorParams(tab, null, idx);
-    addNavigatorButtons(tab, params.drawer);
-    addNavigationStyleParams(tab);
-    addTabIcon(tab);
-    if (!tab.passProps) {
-      tab.passProps = params.passProps;
+    if (tab.components) {
+      const components = tab.components;
+      const screen = createBottomTabScreen(components[0], idx, params)
+      const {label, icon} = components[0];
+      components.shift();
+      console.log('guyca', `${JSON.stringify(components[0])}`);
+      screen.screens = components.map(c => createBottomTabScreen({...c, icon, label}, idx, params));
+      
+      newTabs.push(screen);
+    } else {
+      newTabs.push(createBottomTabScreen(tab, idx, params));
     }
-
-    adaptTopTabs(tab, tab.navigatorID);
-
-    tab.screenId = tab.screen;
-
-    let newtab = adaptNavigationStyleToScreenStyle(tab);
-    newtab = adaptNavigationParams(tab);
-    newtab.overrideBackPress = tab.overrideBackPress;
-    newtab.timestamp = Date.now();
-    newTabs.push(newtab);
   });
   params.tabs = newTabs;
 
@@ -326,6 +334,26 @@ async function startTabBasedApp(params) {
 
   return await newPlatformSpecific.startApp(params);
 }
+
+function createBottomTabScreen(tab, idx, params) {
+  addNavigatorParams(tab, null, idx);
+  addNavigatorButtons(tab, params.drawer);
+  addNavigationStyleParams(tab);
+  addTabIcon(tab);
+  if (!tab.passProps) {
+    tab.passProps = params.passProps;
+  }
+
+  adaptTopTabs(tab, tab.navigatorID);
+
+  tab.screenId = tab.screen;
+
+  let newtab = adaptNavigationStyleToScreenStyle(tab);
+  newtab = adaptNavigationParams(tab);
+  newtab.overrideBackPress = tab.overrideBackPress;
+  newtab.timestamp = Date.now();
+  return newtab;
+};
 
 function addTabIcon(tab) {
   if (tab.icon) {
@@ -553,7 +581,6 @@ function addNavigatorParams(screen, navigator = null, idx = '') {
 }
 
 function addNavigatorButtons(screen, sideMenuParams) {
-
   const Screen = Navigation.getRegisteredScreen(screen.screen);
   if (screen.navigatorButtons == null) {
     screen.navigatorButtons = _.cloneDeep(Screen.navigatorButtons);

commit 0cb611f7e62ff903cde9bebfd49d5b1df53ed668

Fix flickering modal on Android
The initial modal screen got animated twice, this regression was introduced in #2761.

android/app/src/main/java/com/reactnativenavigation/layouts/ModalScreenLayout.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import com.reactnativenavigation.params.ScreenParams;
 import com.reactnativenavigation.views.LeftButtonOnClickListener;
 
+import java.util.List;
+
 public class ModalScreenLayout extends SingleScreenLayout {
 
     public ModalScreenLayout(AppCompatActivity activity,
@@ -16,6 +18,23 @@ public class ModalScreenLayout extends SingleScreenLayout {
 
     @Override
     protected void pushInitialScreen(LayoutParams lp) {
-        stack.pushInitialModalScreenWithAnimation(screenParams, lp);
+        if (screenParams.screens.isEmpty()) {
+            stack.pushInitialModalScreenWithAnimation(screenParams, lp);
+        } else {
+            stack.pushInitialScreen(screenParams, lp);
+        }
+    }
+
+    @Override
+    protected void pushAdditionalScreens(LayoutParams lp) {
+        List<ScreenParams> screens = screenParams.screens;
+        for (int i = 0, screensSize = screens.size(); i < screensSize; i++) {
+            ScreenParams screen = screens.get(i);
+            if (i == screens.size() - 1) {
+                stack.pushInitialModalScreenWithAnimation(screen, lp);
+            } else {
+                stack.pushInitialScreen(screen, lp);
+            }
+        }
     }
 }

android/app/src/main/java/com/reactnativenavigation/layouts/SingleScreenLayout.java
@@ -86,17 +86,17 @@ public class SingleScreenLayout extends BaseLayout {
         LayoutParams lp = new LayoutParams(MATCH_PARENT, MATCH_PARENT);
         pushInitialScreen(lp);
         pushAdditionalScreens(lp);
-        stack.show(NavigationType.Push);
     }
 
     protected void pushInitialScreen(LayoutParams lp) {
         stack.pushInitialScreen(screenParams, lp);
     }
 
-    private void pushAdditionalScreens(LayoutParams lp) {
+    protected void pushAdditionalScreens(LayoutParams lp) {
         for (ScreenParams screen : screenParams.screens) {
             stack.pushInitialScreen(screen, lp);
         }
+        stack.show(NavigationType.Push);
     }
 
     private void sendScreenChangedEventAfterInitialPush() {