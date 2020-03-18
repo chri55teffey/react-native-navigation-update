chri55teffey/react-native-navigation-update

commit 879d4ff59154b963ec4e98242f0f3e0abc9e67d2

Stop rejecting on dismissAllModals if no modals are displayed (#5991)
This commit adds parity with iOS which does not reject the promise if dismissAllModals is called while no modals are displayed.

lib/android/app/src/main/java/com/reactnativenavigation/viewcontrollers/modal/ModalStack.java
@@ -84,10 +84,8 @@ public class ModalStack {
 
     public void dismissAllModals(ViewController root, Options mergeOptions, CommandListener listener) {
         if (modals.isEmpty()) {
-            listener.onError("Nothing to dismiss");
             return;
         }
-
         String topModalId = peek().getId();
         String topModalName = peek().getCurrentComponentName();
         int modalsDismissed = size();

lib/android/app/src/test/java/com/reactnativenavigation/viewcontrollers/modal/ModalStackTest.java
@@ -1,11 +1,11 @@
 package com.reactnativenavigation.viewcontrollers.modal;
 
 import android.app.Activity;
-import androidx.coordinatorlayout.widget.CoordinatorLayout;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 
-import com.reactnativenavigation.*;
+import com.reactnativenavigation.BaseTest;
+import com.reactnativenavigation.TestUtils;
 import com.reactnativenavigation.anim.ModalAnimator;
 import com.reactnativenavigation.mocks.SimpleViewController;
 import com.reactnativenavigation.parse.Options;
@@ -14,12 +14,14 @@ import com.reactnativenavigation.utils.CommandListener;
 import com.reactnativenavigation.utils.CommandListenerAdapter;
 import com.reactnativenavigation.viewcontrollers.ChildControllersRegistry;
 import com.reactnativenavigation.viewcontrollers.ViewController;
-import com.reactnativenavigation.viewcontrollers.stack.*;
+import com.reactnativenavigation.viewcontrollers.stack.StackController;
 
 import org.junit.Test;
 import org.mockito.Mockito;
 
 import java.util.EmptyStackException;
+
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
 
 import static org.assertj.core.api.Java6Assertions.assertThat;
 import static org.assertj.core.api.Java6Assertions.assertThatThrownBy;
@@ -169,13 +171,6 @@ public class ModalStackTest extends BaseTest {
         uut.dismissAllModals(root, Options.EMPTY, listener);
         verify(listener, times(1)).onSuccess(anyString());
         verifyZeroInteractions(listener);
-    }
-
-    @Test
-    public void dismissAllModals_rejectIfEmpty() {
-        CommandListener spy = spy(new CommandListenerAdapter());
-        uut.dismissAllModals(root, Options.EMPTY, spy);
-        verify(spy, times(1)).onError(any());
     }
 
     @Test

commit 217f0d547570cbe58e75a8664f02593c2c972d39

Always resolve dismissAllModals promise (#6054)
When no modals are displayed and dismissAllModals is called, resolve the promise successfully. This regression was introduced in #5991

lib/android/app/src/main/java/com/reactnativenavigation/viewcontrollers/modal/ModalStack.java
@@ -84,6 +84,7 @@ public class ModalStack {
 
     public void dismissAllModals(ViewController root, Options mergeOptions, CommandListener listener) {
         if (modals.isEmpty()) {
+            listener.onSuccess(root.getId());
             return;
         }
         String topModalId = peek().getId();

lib/android/app/src/test/java/com/reactnativenavigation/viewcontrollers/modal/ModalStackTest.java
@@ -174,6 +174,13 @@ public class ModalStackTest extends BaseTest {
     }
 
     @Test
+    public void dismissAllModals_resolveSuccessfullyIfEmpty() {
+        CommandListener spy = spy(new CommandListenerAdapter());
+        uut.dismissAllModals(root, Options.EMPTY, spy);
+        verify(spy, times(1)).onSuccess(root.getId());
+    }
+
+    @Test
     public void dismissAllModals_optionsAreMergedOnTopModal() {
         uut.showModal(modal1, root, new CommandListenerAdapter());
         uut.showModal(modal2, root, new CommandListenerAdapter());

lib/android/app/src/test/java/com/reactnativenavigation/viewcontrollers/navigator/NavigatorTest.java
@@ -596,6 +596,7 @@ public class NavigatorTest extends BaseTest {
     public void dismissAllModals_onViewAppearedInvokedOnRoot() {
         disablePushAnimation(child2);
         disableShowModalAnimation(child1);
+        uut.setRoot(child3, new CommandListenerAdapter(), reactInstanceManager);
 
         uut.dismissAllModals(Options.EMPTY, new CommandListenerAdapter());
         verify(parentVisibilityListener, times(0)).onViewAppeared(parentController.getView());